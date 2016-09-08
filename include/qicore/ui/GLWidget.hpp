1a425358a6c0c7d5b83540c6433c1583d5db8286
Mark Roszko <mark.roszko@gmail.com>
2016-09-07 21:09:21 -0400

    Add\setup nanovg to be included in the build and make it draw a box!

--- include/qicore/ui/GLWidget.hpp
+++ include/qicore/ui/GLWidget.hpp
@@ -4,6 +4,9 @@
 #include <QtOpenGL>
 #include "qicore/qicore.hpp"
 
+//forward decl as we dont want to expose nanovg
+struct NVGcontext;
+
 namespace qicore {
 namespace ui {
     class QICORE_EXPORT GLWidget : public QOpenGLWidget {
@@ -12,11 +15,14 @@ namespace ui {
 
     public:
         GLWidget(QWidget *parent): QOpenGLWidget(parent) {}
+        ~GLWidget();
         
     protected:
         void initializeGL() override;
         void resizeGL(int width, int height) override;
         void paintGL() override;
+        
+        struct NVGcontext* nanovg_;
     };
 }
 }
--- src/qicore/ui/GLWidget.cpp
+++ src/qicore/ui/GLWidget.cpp
@@ -1,37 +1,39 @@
-#include "qicore/ui/GLWidget.hpp"
-#include <gl/GLU.h>
+#define NANOVG_GL2_IMPLEMENTATION
+
+#include <gl/glew.h>
 #include <gl/GL.h>
+#include <gl/GLU.h>
+#include "nanovg.h"
+#include "nanovg_gl.h"
+#include "qicore/ui/GLWidget.hpp"
+
 
 using namespace qicore::ui;
 
+GLWidget::~GLWidget() {
+	nvgDeleteGL2(nanovg_);
+}
+
 void GLWidget::initializeGL() {
+    glewInit();
     glClearColor(0,0,0,1);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
-    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-    glEnable(GL_COLOR_MATERIAL);
+//    glEnable(GL_DEPTH_TEST);
+//    glEnable(GL_LIGHT0);
+//    glEnable(GL_LIGHTING);
+    nanovg_ = nvgCreateGL2(NVG_ANTIALIAS | NVG_STENCIL_STROKES);
 }
 
 void GLWidget::resizeGL(int width, int height) {
     glViewport(0,0,width,height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45, (float)width/height, 0.01, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0,0,5,0,0,0,0,1,0);
 }
 
 void GLWidget::paintGL() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+    nvgBeginFrame(nanovg_, width(), height(), devicePixelRatio());
+    nvgBeginPath(nanovg_);
+    nvgRect(nanovg_, 100,100, 120,30);
+    nvgFillColor(nanovg_, nvgRGBA(255,192,0,255));
+    nvgFill(nanovg_);
 
-    glBegin(GL_TRIANGLES);
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex3f(-0.5, -0.5, 0);
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex3f( 0.5, -0.5, 0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3f( 0.0,  0.5, 0);
-    glEnd();
+    nvgEndFrame(nanovg_);
 }
