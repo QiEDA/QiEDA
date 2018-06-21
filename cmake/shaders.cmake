a359bc2d7ac870ee7e1b12fb635a0a1cd68f5f16
Mark Roszko <mark.roszko@gmail.com>
2018-06-20 20:17:24 -0400

    Make shader cmake module smarter and use a hash file to track state

--- cmake/shaders.cmake
+++ cmake/shaders.cmake
@@ -19,15 +19,34 @@ foreach (file ${FILES})
     foreach (line ${lines})
         set(source "${source}    \"${line}\\n\"\n")
     endforeach()
-    
-    
-    set(destfilename "${name}_${extension}.h")
-    file(
-        WRITE ${DESTINATION_DIR}/${destfilename}
-        "static const char *${name}_${extension}_shader = {\n"
-        "${source}"
-        "};\n"
-    )
-    
-    message(STATUS "Shader converted: ${srcfile} to ${DESTINATION_DIR}/${destfilename}")
+
+    string(CONCAT source "static const char *${name}_${extension}_shader = {\n"
+            "${source}"
+            "};\n")
+
+    string(SHA256 sourceHash "${source}")
+
+    set(destHashFileName "${name}_${extension}.hash")
+
+
+    set(existingDestHash "")
+    file (STRINGS ${DESTINATION_DIR}/${destHashFileName} existingDestHash)
+    if(NOT sourceHash STREQUAL existingDestHash)
+        file(
+                WRITE ${DESTINATION_DIR}/${destHashFileName}
+                "${sourceHash}"
+        )
+
+
+        set(destFileName "${name}_${extension}.h")
+        file(
+                WRITE ${DESTINATION_DIR}/${destFileName}
+                "${source}"
+        )
+
+        message(STATUS "Shader ${srcfile} converted to ${DESTINATION_DIR}/${destFileName}, hash: ${sourceHash}")
+    else()
+        message(STATUS "Shader changed not detected for ${srcfile}, hash: ${sourceHash}")
+    endif()
+
 endforeach()
